43c2ccf103d0ce6c1ab4743ce84e8044fcebe4be
Yahye Abdirahman <68783841+fikrado@users.noreply.github.com>
2021-01-19 07:14:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import tweepy
 import time
 
-
 consumer_key = 'hU4anbsA7eNHHgkhKqIH3uApk'
 consumer_secret = 'wH0Y1lcjieVHvp9ZWdc02peZXPoqd4y6t6cRo3rVJm9u6ypSSK'
 
@@ -13,14 +12,82 @@ auth.set_access_token(key, secret)
 
 api = tweepy.API(auth)
 
-search = 'python, somali, ethicalhacker, cyberscurity, CCNA, CCNP, somaliland, pentester, tech, it, un, unitednations '
-nrTweets = 500
+search = 'somali'
+search2 = 'hacking'
+search3 = 'viral'
+search4 = 'somaliland'
+search5 = 'love'
+search6 = 'cats'
+search7 = 'code'
+
+nrTweets = 50
+
 
 for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
     try:
         tweet.favorite()
         tweet.retweet()
-        time.sleep(11000)
+        time.sleep(10)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+for tweet in tweepy.Cursor(api.search, search4).items(nrTweets):
+    try:
+        tweet.favorite()
+        tweet.retweet()
+        time.sleep(10)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+for tweet in tweepy.Cursor(api.search, search2).items(nrTweets):
+    try:
+        tweet.favorite()
+        tweet.retweet()
+        time.sleep(10)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+for tweet in tweepy.Cursor(api.search, search3).items(nrTweets):
+    try:
+        tweet.favorite()
+        tweet.retweet()
+        time.sleep(10)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+for tweet in tweepy.Cursor(api.search, search5).items(nrTweets):
+    try:
+        tweet.favorite()
+        tweet.retweet()
+        time.sleep(10)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+for tweet in tweepy.Cursor(api.search, search6).items(nrTweets):
+    try:
+        tweet.favorite()
+        tweet.retweet()
+        time.sleep(10)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+for tweet in tweepy.Cursor(api.search, search7).items(nrTweets):
+    try:
+        tweet.favorite()
+        tweet.retweet()
+        time.sleep(10)
     except tweepy.TweepError as e:
         print(e.reason)
     except StopIteration:
